d45155edd269803002a4ff18c4438d6e39745e4b
Andy Marke <andy.marke@gmail.com>
2020-01-17 17:34:10 +0000

    DT-461 Fix appinsights conditional logic to use APPLICATION_INSIGHTS_IKEY

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/config/ApplicationInsightsConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/config/ApplicationInsightsConfiguration.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.prisontoprobation.config
 
 import com.microsoft.applicationinsights.TelemetryClient
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.*
 import org.springframework.core.type.AnnotatedTypeMetadata
 
@@ -12,25 +10,17 @@ import org.springframework.core.type.AnnotatedTypeMetadata
  */
 @Configuration
 open class ApplicationInsightsConfiguration {
-    companion object {
-        val log: Logger = LoggerFactory.getLogger(this::class.java)
-    }
-
-
     @Bean
     @Conditional(AppInsightKeyAbsentCondition::class)
     open fun telemetryClient(): TelemetryClient {
-        log.info("Creating TelemetryClient since no appinsights.instrumentationkey found")
         return TelemetryClient()
     }
 
     class AppInsightKeyAbsentCondition : Condition {
 
         override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
-            val telemetryKey: String? = context.environment.getProperty("appinsights.instrumentationkey")
-            val noAppInsightsKey = telemetryKey.isNullOrBlank()
-            log.info("app insights key has blank status $noAppInsightsKey, length was ${telemetryKey?.length}")
-            return noAppInsightsKey
+            val telemetryKey: String? = context.environment.getProperty("application.insights.ikey")
+            return telemetryKey.isNullOrBlank()
         }
     }
 }
