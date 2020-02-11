eb2955afecf0c3bbbf5c24af89d4112ce09b5de1
Andy Marke <andy.marke@gmail.com>
2020-02-11 09:57:39 +0000

    DT-466 Create skeleton services and tests

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/services/BookingChangeService.kt
@@ -0,0 +1,48 @@
+package uk.gov.justice.digital.hmpps.prisontoprobation.services
+
+import com.microsoft.applicationinsights.TelemetryClient
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+
+@Service
+open class BookingChangeService(private val telemetryClient: TelemetryClient
+) {
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  open fun checkBookingCreationAndUpdateProbation(message: OffenderBookingInsertedMessage) {
+    val (bookingId, offenderId) = message
+    val trackingAttributes = mapOf(
+        "bookingId" to bookingId.toString(),
+        "offenderId" to offenderId.toString())
+
+    log.info("Booking $bookingId created for offender $offenderId")
+    telemetryClient.trackEvent("P2PBookingCreated", trackingAttributes, null)
+  }
+
+  open fun checkBookingReassignedAndUpdateProbation(message: OffenderBookingReassignedMessage) {
+    val (bookingId, offenderId, previousOffenderId) = message
+    val trackingAttributes = mapOf(
+        "bookingId" to bookingId.toString(),
+        "offenderId" to offenderId.toString(),
+        "previousOffenderId" to previousOffenderId.toString())
+
+    log.info("Booking $bookingId reassigned from offender $previousOffenderId to $offenderId")
+    telemetryClient.trackEvent("P2PBookingReassigned", trackingAttributes, null)
+  }
+
+  open fun checkBookingNumberChangedAndUpdateProbation(message: BookingNumberChangedMessage) {
+    val (bookingId: Long, offenderId: Long, bookingNumber: String, previousBookingNumber: String) = message
+    val trackingAttributes = mapOf(
+        "bookingId" to bookingId.toString(),
+        "offenderId" to offenderId.toString(),
+        "bookingNumber" to bookingNumber,
+        "previousBookingNumber" to previousBookingNumber)
+
+    log.info("Booking $bookingId has booking number changed to $bookingNumber")
+    telemetryClient.trackEvent("P2PBookingNumberChanged", trackingAttributes, null)
+  }
+}
