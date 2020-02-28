40f9f4e6aef6b656862d3686924dfc03c274b07a
Andy Marke <andy.marke@gmail.com>
2020-02-28 15:46:19 +0000

    Merge two test classes into one to prevent duplicate mocks causing intermittent test failures

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/MessageIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/MessageIntegrationTest.kt
@@ -9,18 +9,35 @@ import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilCallTo
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
 import org.mockito.Mockito.mockingDetails
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles(profiles = ["test", "test-queue"])
-class ImprisonmentStatusChangeIntegrationTest : QueueIntegrationTest() {
+class MessageIntegrationTest : QueueIntegrationTest() {
 
     @Autowired
     @MockBean
     private lateinit var telemetryClient: TelemetryClient
+
+
+    @Test
+    fun `will consume a prison movement message, update probation and create movement insights event`() {
+        val message = this::class.java.getResource("/messages/externalMovement.json").readText()
+
+        // wait until our queue has been purged
+        await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+
+        awsSqsClient.sendMessage(queueUrl, message)
+
+        await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+        await untilCallTo { eliteRequestCountFor("/api/bookings/1200835/movement/1") } matches { it == 1 }
+        await untilCallTo { eliteRequestCountFor("/api/bookings/1200835?basicInfo=true") } matches { it == 1 }
+        await untilCallTo { eliteRequestCountFor("/api/prisoners?offenderNo=A5089DY") } matches { it == 1 }
+        await untilCallTo { communityPutCountFor("/secure/offenders/nomsNumber/A5089DY/custody/bookingNumber/38339A") } matches { it == 1 }
+        await untilCallTo { mockingDetails(telemetryClient).invocations.size } matches { it == 2 }
+
+        verify(telemetryClient).trackEvent(eq("P2PTransferProbationUpdated"), any(), isNull())
+    }
 
     @Test
     fun `will consume a imprisonment status change message, update probation and create movement insights event`() {
@@ -39,4 +56,5 @@ class ImprisonmentStatusChangeIntegrationTest : QueueIntegrationTest() {
 
         verify(telemetryClient).trackEvent(eq("P2PImprisonmentStatusUpdated"), any(), isNull())
     }
+
 }
