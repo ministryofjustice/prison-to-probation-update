b139e303455631773519b9cd94fa00b1926df799
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-06-19 14:05:40 +0100

    DT-853 Split tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,9 +50,11 @@ tasks.withType<Test> {
 }
 if (System.getProperty("test.profile") == "integration") {
   reporting.baseDir = File("build/reports/tests/integration")
+  project.setProperty("testResultsDirName", "$buildDir/integration")
 }
 
 if (System.getProperty("test.profile") == "unit") {
   reporting.baseDir = File("build/reports/tests/unit")
+  project.setProperty("testResultsDirName", "$buildDir/unit")
 }
 
