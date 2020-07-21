e00bace1d84c3137bb9ceec04bb3130af3685d96
Peter Phillips <peter@greenthistle.co.uk>
2020-07-21 06:56:01 +0100

    DT-1026: Remove dependency on less as no longer in base image

--- src/test/resources/localstack/setup-sns.sh
+++ src/test/resources/localstack/setup-sns.sh
@@ -4,6 +4,8 @@ export TERM=ansi
 export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
+export PAGER=
+
 aws --endpoint-url=http://localhost:4575 sns create-topic --name offender_events
 
 aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name prison_to_probation_dlq
