0fc2ff96519e146748870b24c4e23fe2bfe8280c
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-02-04 07:40:35 +0000

    DT-1586 Allow import of swagger docs in postman (#152)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/config/OpenApiConfiguration.kt
@@ -8,11 +8,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfiguration {
+class OpenApiConfiguration(buildProperties: BuildProperties) {
+  private val version: String = buildProperties.version
+
   @Bean
-  fun customOpenAPI(buildProperties: BuildProperties): OpenAPI = OpenAPI()
+  fun customOpenAPI(): OpenAPI = OpenAPI()
     .info(
       Info().title("Prison to Probation update service")
+        .version(version)
         .description("A service that synchronises prison custody data to Delius")
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk"))
     )
