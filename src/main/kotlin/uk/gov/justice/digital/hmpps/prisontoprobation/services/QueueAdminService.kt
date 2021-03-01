13c5700f878a601d9f887aa7420d4231ed18c2a3
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2021-03-01 13:47:46 +0000

    DT-1561 Set up administration of dead letter queue (#159)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/services/QueueAdminService.kt
@@ -0,0 +1,49 @@
+package uk.gov.justice.digital.hmpps.prisontoprobation.services
+
+import com.amazonaws.services.sqs.AmazonSQS
+import com.amazonaws.services.sqs.model.DeleteMessageRequest
+import com.amazonaws.services.sqs.model.PurgeQueueRequest
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest
+import com.google.gson.Gson
+import com.microsoft.applicationinsights.TelemetryClient
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class QueueAdminService(
+  private val awsSqsClient: AmazonSQS,
+  private val awsSqsDlqClient: AmazonSQS,
+  private val telemetryClient: TelemetryClient,
+  @Value("\${sqs.queue.name}") private val eventQueueName: String,
+  @Value("\${sqs.dlq.name}") private val eventDlqName: String,
+  private val gson: Gson
+) {
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  val eventQueueUrl: String by lazy { awsSqsClient.getQueueUrl(eventQueueName).queueUrl }
+  val eventDlqUrl: String by lazy { awsSqsDlqClient.getQueueUrl(eventDlqName).queueUrl }
+
+  fun clearAllDlqMessagesForEvent() {
+    awsSqsDlqClient.purgeQueue(PurgeQueueRequest(eventDlqUrl))
+    log.info("Clear all messages on event dead letter queue")
+  }
+
+  fun transferEventMessages() =
+    repeat(getEventDlqMessageCount()) {
+      awsSqsDlqClient.receiveMessage(ReceiveMessageRequest(eventDlqUrl).withMaxNumberOfMessages(1)).messages
+        .forEach { msg ->
+          awsSqsClient.sendMessage(eventQueueUrl, msg.body)
+          awsSqsDlqClient.deleteMessage(DeleteMessageRequest(eventDlqUrl, msg.receiptHandle))
+        }
+    }
+
+  private fun getEventDlqMessageCount() =
+    awsSqsDlqClient.getQueueAttributes(eventDlqUrl, listOf("ApproximateNumberOfMessages"))
+      .attributes["ApproximateNumberOfMessages"]
+      ?.toInt() ?: 0
+}
