f88a47eb8f23b2403d65d0f74f47ae2e12c08bdd
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-06-15 15:12:11 +0100

    DT-2170 Use new sqs configuration to create AmazonSQS beans and localstack queuees

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/config/JmsConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/config/JmsConfig.kt
@@ -8,15 +8,37 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import com.amazonaws.services.sqs.model.CreateQueueRequest
+import com.amazonaws.services.sqs.model.QueueAttributeName
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jms.annotation.EnableJms
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory
 import org.springframework.jms.support.destination.DynamicDestinationResolver
 import javax.jms.Session
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "hmpps.sqs")
+data class SqsConfigProperties(
+  val region: String,
+  val provider: String,
+  val localstackUrl: String = "",
+  val dpsQueue: QueueConfig,
+  val hmppsQueue: QueueConfig,
+) {
+  data class QueueConfig(
+    val queueName: String,
+    val queueAccessKeyId: String = "",
+    val queueSecretAccessKey: String = "",
+    val dlqName: String,
+    val dlqAccessKeyId: String = "",
+    val dlqSecretAccessKey: String = "",
+  )
+}
 
 @Configuration
 @EnableJms
@@ -45,94 +67,101 @@ class JmsConfig {
   }
 
   @Bean
-  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "aws")
-  fun awsSqsClient(
-    @Value("\${sqs.aws.access.key.id}") accessKey: String,
-    @Value("\${sqs.aws.secret.access.key}") secretKey: String,
-    @Value("\${sqs.endpoint.region}") region: String
-  ): AmazonSQS =
-    AmazonSQSClientBuilder.standard()
-      .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
-      .withRegion(region)
-      .build()
+  @ConditionalOnProperty(name = ["hmpps.sqs.provider"], havingValue = "aws")
+  fun awsSqsClient(sqsConfigProperties: SqsConfigProperties): AmazonSQS =
+    with(sqsConfigProperties) {
+      amazonSQS(dpsQueue.queueAccessKeyId, dpsQueue.queueSecretAccessKey, region)
+        .also { log.info("Created sqs client for queue ${dpsQueue.queueName}") }
+    }
 
   @Bean
-  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "aws")
-  fun hmppsAwsSqsClient(
-    @Value("\${sqs.hmpps.aws.access.key.id}") accessKey: String,
-    @Value("\${sqs.hmpps.aws.secret.access.key}") secretKey: String,
-    @Value("\${sqs.hmpps.endpoint.region}") region: String
-  ): AmazonSQS =
-    AmazonSQSClientBuilder.standard()
-      .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
-      .withRegion(region)
-      .build()
+  @ConditionalOnProperty(name = ["hmpps.sqs.provider"], havingValue = "aws")
+  fun hmppsAwsSqsClient(sqsConfigProperties: SqsConfigProperties): AmazonSQS =
+    with(sqsConfigProperties) {
+      amazonSQS(hmppsQueue.queueAccessKeyId, hmppsQueue.queueSecretAccessKey, region)
+        .also { log.info("Created sqs client for queue ${hmppsQueue.queueName}") }
+    }
 
   @Bean
-  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "aws")
-  fun awsSqsDlqClient(
-    @Value("\${sqs.aws.dlq.access.key.id}") accessKey: String,
-    @Value("\${sqs.aws.dlq.secret.access.key}") secretKey: String,
-    @Value("\${sqs.endpoint.region}") region: String
-  ): AmazonSQS =
-    AmazonSQSClientBuilder.standard()
-      .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
-      .withRegion(region)
-      .build()
+  @ConditionalOnProperty(name = ["hmpps.sqs.provider"], havingValue = "aws")
+  fun awsSqsDlqClient(sqsConfigProperties: SqsConfigProperties): AmazonSQS =
+    with(sqsConfigProperties) {
+      amazonSQS(dpsQueue.dlqAccessKeyId, dpsQueue.dlqSecretAccessKey, region)
+        .also { log.info("Created dlq sqs client for dlq ${dpsQueue.dlqName}") }
+    }
 
   @Bean
-  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "aws")
-  fun hmppsAwsSqsDlqClient(
-    @Value("\${sqs.hmpps.aws.dlq.access.key.id}") accessKey: String,
-    @Value("\${sqs.hmpps.aws.dlq.secret.access.key}") secretKey: String,
-    @Value("\${sqs.hmpps.endpoint.region}") region: String
-  ): AmazonSQS =
+  @ConditionalOnProperty(name = ["hmpps.sqs.provider"], havingValue = "aws")
+  fun hmppsAwsSqsDlqClient(sqsConfigProperties: SqsConfigProperties): AmazonSQS =
+    with(sqsConfigProperties) {
+      amazonSQS(hmppsQueue.dlqAccessKeyId, hmppsQueue.dlqSecretAccessKey, region)
+        .also { log.info("Created dlq sqs client for dlq ${hmppsQueue.dlqName}") }
+    }
+
+  private fun amazonSQS(accessKeyId: String, secretAccessKey: String, region: String) =
     AmazonSQSClientBuilder.standard()
-      .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
+      .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKeyId, secretAccessKey)))
       .withRegion(region)
       .build()
 
   @Bean("awsSqsClient")
-  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "localstack")
-  fun awsSqsClientLocalstack(
-    @Value("\${sqs.endpoint.url}") serviceEndpoint: String,
-    @Value("\${sqs.endpoint.region}") region: String
-  ): AmazonSQS =
-    AmazonSQSClientBuilder.standard()
-      .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
-      .withCredentials(AWSStaticCredentialsProvider(AnonymousAWSCredentials()))
-      .build()
+  @ConditionalOnProperty(name = ["hmpps.sqs.provider"], havingValue = "localstack")
+  fun awsSqsClientLocalstack(sqsConfigProperties: SqsConfigProperties, awsSqsDlqClient: AmazonSQS): AmazonSQS =
+    with(sqsConfigProperties) {
+      localstackAmazonSQS(localstackUrl, region)
+        .also { sqsClient -> createMainQueue(sqsClient, awsSqsDlqClient, dpsQueue.queueName, dpsQueue.dlqName) }
+        .also { log.info("Created localstack sqs client for queue ${dpsQueue.queueName}") }
+    }
 
   @Bean("hmppsAwsSqsClient")
-  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "localstack")
-  fun hmppsAwsSqsClientLocalstack(
-    @Value("\${sqs.hmpps.endpoint.url}") serviceEndpoint: String,
-    @Value("\${sqs.hmpps.endpoint.region}") region: String
-  ): AmazonSQS =
-    AmazonSQSClientBuilder.standard()
-      .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
-      .withCredentials(AWSStaticCredentialsProvider(AnonymousAWSCredentials()))
-      .build()
+  @ConditionalOnProperty(name = ["hmpps.sqs.provider"], havingValue = "localstack")
+  fun hmppsAwsSqsClientLocalstack(sqsConfigProperties: SqsConfigProperties, hmppsAwsSqsDlqClient: AmazonSQS): AmazonSQS =
+    with(sqsConfigProperties) {
+      localstackAmazonSQS(localstackUrl, region)
+        .also { sqsClient -> createMainQueue(sqsClient, hmppsAwsSqsDlqClient, hmppsQueue.queueName, hmppsQueue.dlqName) }
+        .also { log.info("Created localstack sqs client for queue ${hmppsQueue.queueName}") }
+    }
 
   @Bean("awsSqsDlqClient")
-  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "localstack")
-  fun awsSqsDlqClientLocalstack(
-    @Value("\${sqs.endpoint.url}") serviceEndpoint: String,
-    @Value("\${sqs.endpoint.region}") region: String
-  ): AmazonSQS =
+  @ConditionalOnProperty(name = ["hmpps.sqs.provider"], havingValue = "localstack")
+  fun awsSqsDlqClientLocalstack(sqsConfigProperties: SqsConfigProperties): AmazonSQS =
+    with(sqsConfigProperties) {
+      localstackAmazonSQS(localstackUrl, region)
+        .also { dlqSqsClient -> dlqSqsClient.createQueue(dpsQueue.dlqName) }
+        .also { log.info("Created localstack dlq sqs client for dlq ${dpsQueue.dlqName}") }
+    }
+
+  @Bean("hmppsAwsSqsDlqClient")
+  @ConditionalOnProperty(name = ["hmpps.sqs.provider"], havingValue = "localstack")
+  fun hmppsAwsSqsDlqClientLocalstack(sqsConfigProperties: SqsConfigProperties): AmazonSQS =
+    with(sqsConfigProperties) {
+      localstackAmazonSQS(localstackUrl, region)
+        .also { dlqSqsClient -> dlqSqsClient.createQueue(hmppsQueue.dlqName) }
+        .also { log.info("Created localstack dlq sqs client for dlq ${hmppsQueue.dlqName}") }
+    }
+
+  private fun localstackAmazonSQS(localstackUrl: String, region: String) =
     AmazonSQSClientBuilder.standard()
-      .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
+      .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(localstackUrl, region))
       .withCredentials(AWSStaticCredentialsProvider(AnonymousAWSCredentials()))
       .build()
 
-  @Bean("hmppsAwsSqsDlqClient")
-  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "localstack")
-  fun hmppsAwsSqsDlqClientLocalstack(
-    @Value("\${sqs.hmpps.endpoint.url}") serviceEndpoint: String,
-    @Value("\${sqs.hmpps.endpoint.region}") region: String
-  ): AmazonSQS =
-    AmazonSQSClientBuilder.standard()
-      .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
-      .withCredentials(AWSStaticCredentialsProvider(AnonymousAWSCredentials()))
-      .build()
+  private fun createMainQueue(
+    queueSqsClient: AmazonSQS,
+    dlqSqsClient: AmazonSQS,
+    queueName: String,
+    dlqName: String,
+  ) =
+    dlqSqsClient.getQueueUrl(dlqName).queueUrl
+      .let { dlqQueueUrl -> dlqSqsClient.getQueueAttributes(dlqQueueUrl, listOf(QueueAttributeName.QueueArn.toString())).attributes["QueueArn"]!! }
+      .also { queueArn ->
+        queueSqsClient.createQueue(
+          CreateQueueRequest(queueName).withAttributes(
+            mapOf(
+              QueueAttributeName.RedrivePolicy.toString() to
+                """{"deadLetterTargetArn":"$queueArn","maxReceiveCount":"5"}"""
+            )
+          )
+        )
+      }
 }
