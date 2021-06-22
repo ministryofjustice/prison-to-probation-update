21e53079b493f42771c5c24b7b0ffd98870fd21c
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-06-22 11:02:31 +0100

    DT-2215 Ktlint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/services/HMPPSPrisonerChangesListenerPusher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/services/HMPPSPrisonerChangesListenerPusher.kt
@@ -18,10 +18,7 @@ class HMPPSPrisonerChangesListenerPusher(
     val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 
-  @JmsListener(
-    destination = "#{@'hmpps.sqs-uk.gov.justice.digital.hmpps.prisontoprobation.config.SqsConfigProperties'.hmppsQueue.queueName}",
-    containerFactory = "hmppsJmsListenerContainerFactory"
-  )
+  @JmsListener(destination = "#{@'hmpps.sqs-uk.gov.justice.digital.hmpps.prisontoprobation.config.SqsConfigProperties'.queues['hmppsQueue'].queueName}", containerFactory = "hmppsJmsListenerContainerFactory")
   fun pushHMPPSPrisonUpdateToProbation(requestJson: String?) {
     log.debug(requestJson)
     val (message, messageId, messageAttributes) = objectMapper.readValue(requestJson, HMPPSMessage::class.java)
