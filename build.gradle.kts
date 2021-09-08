57e2e1bda15e9b2a446b776378384d7e9c8044b1
Peter Phillips <peter@greenthistle.co.uk>
2021-09-08 08:43:59 +0100

    DT-2510: 🔧 Add upgrade to Dockerfiles and upgrade libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,34 +23,34 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("net.javacrumbs.shedlock:shedlock-spring:4.23.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-dynamodb:4.23.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:4.26.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-dynamodb:4.26.0")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.8")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.4")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.5")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.10")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:0.8.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
   implementation("com.amazonaws:aws-java-sdk-dynamodb")
   implementation("io.github.boostchicken:spring-data-dynamodb:5.2.5")
 
   implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("io.micrometer:micrometer-core")
-  implementation("com.opencsv:opencsv:5.4")
+  implementation("com.opencsv:opencsv:5.5.2")
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.5.0")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.5.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:3.10.0")
+  testImplementation("org.mockito:mockito-inline:3.12.4")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
