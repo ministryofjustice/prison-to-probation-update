33954ffff74ce85fbcea63ff5cac669cc8e77bcb
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-02-28 14:44:43 +0000

    fix some failing tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisontoprobation/config/ResourceServerConfiguration.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.prisontoprobation.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class ResourceServerConfiguration {
   @Bean
   fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -22,7 +24,7 @@ class ResourceServerConfiguration {
             "/webjars/**", "/favicon.ico", "/csrf",
             "/health/**", "/info", "/ping",
             "/prometheus/**",
-            "/v3/api-docs/**", "/swagger-ui/**",
+            "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
             "/swagger-resources", "/swagger-resources/configuration/ui", "/swagger-resources/configuration/security",
             "/queue-admin/retry-all-dlqs",
             "/synthetic-monitor" // This endpoint is secured in the ingress rather than the app so that it can be called from within the namespace without requiring authentication
