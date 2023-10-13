a3c26b991c8305a3d76dcc562397f0bbaef0e0e2
Marcus Aspin <maspin@unilink.com>
2023-10-13 15:53:21 +0000

    Set mockito-inline version

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-  testImplementation("org.mockito:mockito-inline")
+  testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
 }
 
